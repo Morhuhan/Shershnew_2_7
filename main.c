b6f163219347346b5a7412e5bd9034fa406dcebf
John Pigengton <87900147+Morhuhan@users.noreply.github.com>
2022-06-03 11:25:07 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Копирует все символы из файла from в to
 void copy (FILE* from, FILE* to) {
 
     char ch;
 
-    while((ch=getc(from))!=EOF) {
+    int size;
+
+    fseek(from, 0, SEEK_END);
+    size = ftell(from);
+    fseek(from, 0, SEEK_SET);
+
+    printf("size = %d", size);
+
+    for (int i = 0; i < size; i++) {
+        ch=getc(from);
         putc(ch,to);
     }
-
 }
 
 
@@ -18,15 +25,38 @@ void main(int argc, char* argv[]) {
     FILE* from;
     FILE* to;
 
+    if (fopen(argv[3], "r") != NULL) {
+        printf("File with this name already exists!");
+        exit(0);
+    }
+
     if (strcmp(argv[2], "/A") == 0) {
+
         from = fopen(argv[1], "r");
         to = fopen(argv[3], "w");
-        printf("ok!");
+
+        if (from == NULL) {
+            printf("Directory 1 does not exist");
+            exit(0);
+        }
+
+        if (to == NULL) {
+            printf("Directory 2 does not exist");
+            exit(0);
+        }
     }
 
     else if (strcmp(argv[2], "/B") == 0) {
-        from = fopen(argv[1], "rb");
-        to = fopen(argv[3], "wb");
+
+        if (fopen(argv[1], "rb") == NULL) {
+            printf("Directory 1 does not exist");
+            exit(0);
+        }
+
+        if (fopen(argv[3], "wb") == NULL) {
+            printf("Directory 2 does not exist");
+            exit(0);
+        }
     }
 
     else {
